57ed5705cd0a09c3b9179c64e49bf7445e510265
git-san <uyt8525@daum.net>
2022-07-11 17:17:35 +0900

    리엑트 props

--- react/myfirstreact/src/App.js
+++ react/myfirstreact/src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 function Header(props) {
-    console.log('props', props);
+    //console.log('props', props);
     return <header>
     <h1><a href="/">{props.title}</a></h1>
     </header>
 }
 
-function Nav() {
+function Nav(props) {
+    const lis = []
+    for(let i=0; i<props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    }
     return <nav>
         <ol>
-            <li><a href="/read/1">html</a></li>
-            <li><a href="/read/2">css</a></li>
-            <li><a href="/read/3">js</a></li>
+            {lis}
         </ol>
     </nav>
 }
@@ -25,12 +28,17 @@ function Article(props) {
 }
 
 function App() {
-  return (
-    <div>
-        <Header title="REACT"></Header>
-        <Nav></Nav>
-        <Article title="Welcome" body="Hello, WEB"></Article>
-    </div>
+    const topics = [
+        {id:1, title:'html', body:'html is ...'},
+        {id:2, title:'css', body:'css is ...'},
+        {id:3, title:'javascript', body:'javascript is ...'},
+    ]
+    return (
+        <div>
+            <Header title="REACT"></Header>
+            <Nav topics={topics}></Nav>
+            <Article title="Welcome" body="Hello, WEB"></Article>
+        </div>
   );
 }
 
